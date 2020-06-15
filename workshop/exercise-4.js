71ee666616f9ff39d3b9396cc3ca4fc0bad110e5
Johnny Tortorici <johnny@Johnnys-MacBook-Pro.local>
2020-06-15 17:05:12 -0400

    Exercise 4

--- workshop/exercise-4.js
+++ workshop/exercise-4.js
@@ -5,6 +5,9 @@
 // eg:
 // 0, 1, 4, 9, 16, ...
 
-for (let number = 0; number < 12; number++) {
+let square = 0;
+
+for (let number = 0; number <= 12; number++) {
+  square = number * number;
   console.log('the square of ', number, ' is ', square);
 }
